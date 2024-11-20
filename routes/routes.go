f51e6db8e5015dbd0c4c6915f538b120d5cef65c
Rohit-Prasanna <115570423+Rohit-Prasanna@users.noreply.github.com>
2024-11-20 19:41:22 +0530

    Add files via upload

--- routes/routes.go
+++ routes/routes.go
@@ -2,22 +2,17 @@ package routes
 
 import (
 	"github.com/Rohit-Prasanna/todo/handlers"
+
 	"github.com/gorilla/mux"
 )
 
+// InitRoutes initializes all API routes
 func InitRoutes() *mux.Router {
 	router := mux.NewRouter()
 
-	// Fetch all todos
-	router.HandleFunc("/api/todos", handlers.GetTodos).Methods("GET")
-
-	// Create a new todo
+	router.HandleFunc("/api/todos/{userId}", handlers.GetTodos).Methods("GET")
 	router.HandleFunc("/api/todos", handlers.CreateTodo).Methods("POST")
-
-	// Update a todo by ID
 	router.HandleFunc("/api/todos/{id}", handlers.UpdateTodo).Methods("PUT")
-
-	// Delete a todo by ID
 	router.HandleFunc("/api/todos/{id}", handlers.DeleteTodo).Methods("DELETE")
 
 	return router
